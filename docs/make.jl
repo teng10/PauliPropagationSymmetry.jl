50b228590d950a770be56379de03b5704c76eba2
Tyson Jones <tyson.jones.input@gmail.com>
2025-04-15 19:02:32 +0200

    enable doc-gen from non-main branches (#77)
    * testing Documenter.jl fix
    
    why oh why would the default behaviour ignore the triggering branch
    
    * testing whether Documenter.jl uses main files
    
    * permitting PR gen
    
    * temporarily triggering CI from doc-fix branch
    
    so that Manuel is prompted to permit the CI to run for the first time, allowing subsequent manual launches with workflow_dispatch
    
    * testing potential bug workaround
    
    * gh-pages doc didn't update site - why?
    
    * why is gh-pages branch not updating the site?
    
    * updated CI triggers
    
    * updating make.jl comments
    
    to navigate around incredible undocumented Documenter.jl pitfall, ouch!

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,26 @@
+# Generates HTML documentation from the contents of
+# the docs folder, triggered locally by command
+#   julia --project make.jl
+# 
+# If triggered within a Github Action, the generated
+# HTML files will then be committed to the 'gh-pages'
+# branch, which Github Pages can be configured to
+# display at msrudolph.github.io/PauliPropagation.jl/
+# 
+# Note documentation generated from non-main branches
+# will be uploaded to subdomain /dev/, even when not
+# from the 'dev' branch, and doc generated from pull
+# requests will be uploaded to /previews/PR#.
+
+
 using Documenter, PauliPropagation
 
-# determines doc site layout
+
+# Generate doc HTML files, saved to build/
 makedocs(
     sitename="Pauli Propagation",
+
+    # determines site layout
     pages=[
         "index.md",
         "installation.md",
@@ -22,7 +40,22 @@ makedocs(
     ]
 )
 
-# enables doc site deployment to Github Pages
+
+# When run from a Github Action, commit those files to the 'gh-pages' branch.
+# If the action's invoking branch is not main, the files are uploaded under /dev/
 deploydocs(
     repo="github.com/MSRudolph/PauliPropagation.jl.git",
+
+    # Enable generation of doc from PRs, under a /previews/PR## sub-domain.
+    # Beware that this requires the Github Action was explicitly triggered by
+    # a 'pull_request' event (not a 'push')
+    push_preview=true
 )
+
+
+# Once 'gh-pages' branch is updated, and Github Pages has been configured to
+# publish files from that branch, the documentation is visible at either:
+# - msrudolph.github.io/PauliPropagation.jl/
+# - msrudolph.github.io/PauliPropagation.jl/dev/
+# - msrudolph.github.io/PauliPropagation.jl/previews/PR#
+# where # above is replaced with the pull request number
